953502d5ca2b135b9d3e204005bc8a426408d4a1
Abhijeet1125 <abhijeet.iitp25@gmail.com>
2024-01-20 19:36:03 +0530

    Driving car movement Done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict"
-let hi = document.getElementById("playground").clientHeight
+let hi = document.getElementById("playground").clientHeight - 4;
 let wi = document.body.clientWidth
-wi = Math.min(wi, hi);
+wi = Math.min(wi, hi/1.1);
 document.getElementById("playground").style.width = `${wi}px`
 
 
@@ -16,21 +16,61 @@ const canvas = can.getContext('2d')
 const back = new Image()
 back.src = "images/road.png"
 
-let x = 0 ; 
-let imglen = Math.floor((wi /1080 )* 1916)
+let imglen = Math.floor((wi / 1080) * 1916)
+let x =  imglen - 5 ;
+
+let speed = 0;
+
+
+
+let ordi = [
+    {
+        'wid' : wi/ 10 ,
+        'hei' : wi / 5 ,
+        'xx'  : wi / 3,
+        'yy' :  hi - wi /4- 10 ,
+    }
+]
+
+
+let im = new Image ();
+im.src = "images/cars.png"
+
 
 
 
 function animate() {
-    if ( x >= imglen){ x = 0 ; }
+    if (x > imglen) { x -= imglen; }
     canvas.clearRect(0, 0, wi, hi);
-    canvas.drawImage(back, 0 , 0 , 1080 , 1916 , 0, x , wi, imglen);
-    canvas.drawImage(back, 0 , 0 , 1080 , 1916 , 0, x-imglen , wi, imglen);
-    x = x + 1 ; 
+    canvas.drawImage(back, 0, 0, 1080, 1916, 0, x, wi, imglen);
+    canvas.drawImage(back, 0, 0, 1080, 1916, 0, x - imglen, wi, imglen + 2);
+    canvas.drawImage ( im , 15, 22, 60, 155, ordi[0].xx , ordi[0].yy,ordi[0].wid,ordi[0].hei )
+    x += speed;
     requestAnimationFrame(animate);
 }
 animate();
 
 
-
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowUp') {
+        speed += (1 / (speed * 2 + 10));
+        if (speed > 15) { speed = 15; }
+    } else if (event.code === 'ArrowDown') {
+        speed -= 0.01 * speed + 0.1;
+        if (speed <= 0) {
+            speed = 0;
+        }
+    }
+    if (event.code === 'ArrowLeft') {
+        ordi[0].xx -= 2 ; 
+        if ( ordi[0].xx  < wi / 9){
+            ordi [0].xx = wi / 9 ;
+        } 
+    } else if (event.code === 'ArrowRight') {
+        ordi [0].xx += 2  ;
+        if ( ordi[0].xx  > 71 *wi / 90 ){
+            ordi [0].xx = 71 *wi / 90;
+        } 
+    }
+});
 
